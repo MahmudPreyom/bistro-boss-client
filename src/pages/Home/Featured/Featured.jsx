a0c5003d7063fa88b402aa32d3b2d34e75ba6761
MahmudPreyom <preyom5076@gmail.com>
2023-05-30 12:01:01 +0600

    m-74 finished

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -5,9 +5,9 @@ import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white pt-8 my-20">
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle subHeading="Check it out" heading="Featured Item"></SectionTitle>
-            <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+            <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-60 pb-20 pt-12 px-36">
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -15,7 +15,7 @@ const Featured = () => {
                     <p>Aug 20, 2029</p>
                     <p className="uppercase">Where Can i get some</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam deserunt reiciendis, mollitia omnis ex, molestiae unde libero, esse commodi nemo quaerat natus fugit? Autem sapiente neque a? Quae soluta molestias aliquam iure aspernatur sit facilis ex vel. Enim minus quod odit incidunt animi magni libero totam. Hic consequuntur magni culpa!</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
         </div>
